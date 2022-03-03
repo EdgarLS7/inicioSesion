b0c59549a73d6b552af7f3d7a05f4b15ea19a2f6
Edgar Lopez <16171409@itculiacan.edu.mx>
2022-03-03 14:23:53 -0700

    mostrar productos aunque no tenga imagen

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -130,11 +130,14 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
-          //TODO Fix productos cuando no hay imagen
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage(url!),
+        child: url == null
+          ? Image(image: AssetImage('assets/no-image.png'),
           fit: BoxFit.cover,
+        )
+          : FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(url!),
+            fit: BoxFit.cover,
         ),
       ),
     );
